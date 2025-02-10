907f80bed3fc049ae824e669a676be767e70742b
Hyunghui Kim <realwind2048@gmail.com>
2025-02-11 00:26:42 +0900

    Add bg images

--- app/share/[verse-card]/page.tsx
+++ app/share/[verse-card]/page.tsx
@@ -1,5 +1,10 @@
+import Image from 'next/image';
+
 function generateStaticParams() {}
  
 export default function Page() {
-  return <h1>Hello, verse-card Page!</h1>
+  return <div>
+        <Image src="/images/bg_1.webp" alt="image" width="100" height="100"/>
+        <h1>Hello, verse-card Page!</h1>
+    </div>
 }
