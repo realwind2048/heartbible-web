8e40b8f3c206ebcb4886e7304c408e207f6aa61b
Hyunghui Kim <realwind2048@gmail.com>
2025-02-11 01:51:31 +0900

    Apply google roboto font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+    weight: '400',
+    subsets: ['latin'],
+})
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={roboto.className}>
       <body>{children}</body>
     </html>
   )
