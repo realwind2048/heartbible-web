707d00662b28dbe7ac2727fa2dc036ab3d99f3ce
Hyunghui Kim <realwind2048@gmail.com>
2025-02-11 01:44:30 +0900

    Polish share page

--- app/share/[verse-card]/page.tsx
+++ app/share/[verse-card]/page.tsx
@@ -3,8 +3,18 @@ import Image from 'next/image';
 function generateStaticParams() {}
  
 export default function Page() {
-  return <div>
-        <Image src="/images/bg_1.webp" alt="image" width="100" height="100"/>
-        <h1>Hello, verse-card Page!</h1>
+  return (
+    <div>
+        <Image 
+            src="/images/bg_1.webp" 
+            alt="image" 
+            width={100}
+            height={100}
+            style={{objectFit: "cover"}}
+        />
+        <div>
+            <h1 color="black">Hello, verse-card Page!</h1>
+        </div>
     </div>
+  )
 }
