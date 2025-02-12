3dc0cfe26a958012510385015a841e02415ada1d
Hyunghui Kim <realwind2048@gmail.com>
2025-02-12 19:17:04 +0900

    remove shadow

--- app/share/[verseCard]/page.tsx
+++ app/share/[verseCard]/page.tsx
@@ -13,7 +13,7 @@ export default function Page() {
       <div className="relative bg-[url('/images/bg/bg_1.webp')] w-full h-full bg-cover bg-center brightness-50">
       </div>
       <div className="absolute top-0 left-0 flex h-screen w-full h-full">
-        <div className={`m-auto max-w-sm rounded overflow-hidden shadow-lg`}>
+        <div className={`m-auto max-w-sm rounded overflow-hidden`}>
           <div className="px-6 py-4">
             <div className="text-xl mb-2 text-white">
               예수께서 대답하여 이르시되 진실로 진실로 내게 이르노니 사람이 거듭나지 아니하면 하나님의 나라를 볼 수 없느니라
