14d0814d06527d56c2ec92568a7d6b121e943b82
Hyunghui Kim <realwind2048@gmail.com>
2025-02-13 02:34:38 +0900

    Fix className

--- app/components/ShareButton.tsx
+++ app/components/ShareButton.tsx
@@ -5,7 +5,7 @@ export function ShareButton() {
     return (
         <div className="absolute flex w-full bottom-40 items-center justify-center p-5">
             <button 
-                class="inline-flex h-12 items-center justify-center rounded-md bg-neutral-950 px-6 font-medium text-neutral-50 shadow-lg shadow-neutral-500/20 transition active:scale-95"
+                className="inline-flex h-12 items-center justify-center rounded-md bg-neutral-950 px-6 font-medium text-neutral-50 shadow-lg shadow-neutral-500/20 transition active:scale-95"
                 onClick={() => {alert("test");}}>
                 <ShareIcon />
                 <span className="ml-3">공유하기</span>
