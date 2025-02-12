74015581bab10309ac63db55a3d7878dc97e31f5
Hyunghui Kim <realwind2048@gmail.com>
2025-02-13 02:22:32 +0900

    test sharebutton onclick

--- app/components/ShareButton.tsx
+++ app/components/ShareButton.tsx
@@ -4,7 +4,9 @@ import { ShareIcon } from './ShareIcon';
 export function ShareButton() {
     return (
         <div className="absolute flex w-full bottom-40 items-center justify-center p-5">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full inline-flex items-center">
+            <button 
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full inline-flex items-center"
+                onClick={() => {alert("test");}}>
                 <ShareIcon />
                 <span className="ml-3">공유하기</span>
             </button>
