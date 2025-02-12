8909c004add88053b056e98bd409105ac008d327
Hyunghui Kim <realwind2048@gmail.com>
2025-02-13 02:04:00 +0900

    Add icon for 공유하기 버튼

--- app/components/ShareButton.tsx
+++ app/components/ShareButton.tsx
@@ -3,8 +3,9 @@ import Image from 'next/image';
 export function ShareButton() {
     return (
         <div className="absolute flex w-full bottom-40 items-center justify-center p-5">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                공유하기
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full inline-flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><circle cx="18" cy="5" r="3"></circle><circle cx="6" cy="12" r="3"></circle><circle cx="18" cy="19" r="3"></circle><line x1="8.59" y1="13.51" x2="15.42" y2="17.49"></line><line x1="15.41" y1="6.51" x2="8.59" y2="10.49"></line></svg>
+                <span className="ml-3">공유하기</span>
             </button>
         </div>
     );
