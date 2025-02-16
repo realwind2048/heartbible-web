cca2f95bde024c3cd075617239654efa9d7bb525
Hyunghui Kim <realwind2048@gmail.com>
2025-02-17 02:13:13 +0900

    Add link for HeratBibleSignTextLogo

--- app/components/HeartBibleSignTextLogo.tsx
+++ app/components/HeartBibleSignTextLogo.tsx
@@ -1,14 +1,18 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export function HeratBibleSignTextLogo() {
     return (
-        <div className="absolute flex items-center p-5">
-            <p className="text-xl text-white">마음말씀</p>
-            <Image 
-                src="/images/icon/ic_app_icon_transparent_bg.png" 
-                width={25} 
-                height={25} 
-                alt="Heart Bible Logo"/>
-        </div>
+        <Link
+            href="/">
+            <div className="absolute flex items-center p-5">
+                <p className="text-xl text-white">마음말씀</p>
+                <Image 
+                    src="/images/icon/ic_app_icon_transparent_bg.png" 
+                    width={25} 
+                    height={25} 
+                    alt="Heart Bible Logo"/>
+            </div>
+        </Link>
     );
   }
