7d807033d74a919cd7a90db61c3198ca3b1b113f
Hyunghui Kim <realwind2048@gmail.com>
2025-02-18 01:35:16 +0900

    Add todo

--- app/share/versecard/page.tsx
+++ app/share/versecard/page.tsx
@@ -7,6 +7,7 @@ import { Suspense } from 'react'
  * 정보를 받아서 VerseCard 컴포넌트를 렌더링하는 페이지
  */
 export default function Page() {
+  // TODO FIXME heartBibleVerse 는 랜덤으로 보여주므로 공유하기할 때 URL 을 공유하면 받은 사람은 다른 정보가 보임..
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <VerseCardForRandom />
