3e3d22f69de782fa9d3de5e396852233568c106c
Hyunghui Kim <realwind2048@gmail.com>
2025-02-19 00:43:43 +0900

    remove console for production build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production", // 프로덕션에서 콘솔 제거
+    // removeConsole: false, // 콘솔 로그 완전히 제거
+  },
 };
 
 export default nextConfig;
