681c021032473d26e72d311d7d9950a849a06c7b
bruce.kim <bruce.kim@buzzvil.com>
2025-02-20 13:27:51 +0900

    WIP updating VerseFeedItem

--- app/components/feed/VerseFeedItem.tsx
+++ app/components/feed/VerseFeedItem.tsx
@@ -2,6 +2,8 @@
 
 import Link from 'next/link'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import { getRandomBackgroundId } from '@/app/lib/BackgroundUseCase';
 
 export interface VerseFeedItemProps {
     verseString: string;
@@ -9,33 +11,46 @@ export interface VerseFeedItemProps {
 }
 
 export function VerseFeedItem({ verseString, indexString }: VerseFeedItemProps) {
+    const [bgId, setBgId] = useState<number | null>(null);
+
+    useEffect(() => {
+        console.log('Page useEffect');
+        setBgId(getRandomBackgroundId());
+    }, [])
+
     return (
         <>
-            {/* TODO fix Link */}
             <Link 
-                    href={{
+                href={{
                     pathname: `/share/heartbible/random`,
-                    }}>
-                <div className="m-auto max-w-sm rounded overflow-hidden shadow-lg">
-                <Image 
-                    className="object-cover" 
-                    src="/images/image_bible.webp" 
-                    style={{ aspectRatio: '3/2' }} 
-                    alt="VerseCard Background Image"
-                    width={600}
-                    height={400}
-                />
-                <div className="px-6 py-4">
-                    <div className="font-bold text-xl mb-2">말씀 카드</div>
-                    <p className="text-gray-700 text-base">
-                        {verseString}
-                        {indexString}
-                    </p>
-                </div>
-                <div className="px-6 pt-4 pb-2">
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#말씀카드</span>
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#공유</span>
-                </div>
+                }}>
+                <div className="relative m-auto max-w-sm rounded overflow-hidden shadow-lg">
+                    <div 
+                        className={`relative top-0 left-0 brightness-50 h-300`} 
+                        style={{ aspectRatio: '3/2' }} >
+                        <Image 
+                            src={bgId ? `/images/bg/bg_${bgId}.webp` : '/images/image_bible.webp'}
+                            fill
+                            style={{ objectFit: 'cover' }} 
+                            alt="VerseCard Background Image"/>
+                    </div>
+                    <div className="absolute basis-full top-0 left-0">
+                        <div className={`m-auto max-w-sm rounded`}>
+                            <div className="px-6 py-4">
+                                <div className="text-xl mb-2 text-white">
+                                    {verseString}
+                                </div>
+                                <hr className="w-10 h-px mx-auto my-4 bg-gray-100 border-0 rounded-sm md:my-10 dark:bg-gray-700"></hr>
+                                <p className="text-white text-base">
+                                    {indexString}
+                                </p>
+                            </div>
+                        </div>
+                        <div className="basis-full px-6 pt-4 pb-2">
+                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#말씀카드</span>
+                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#공유</span>
+                        </div>
+                    </div>
                 </div>
             </Link>
         </>
