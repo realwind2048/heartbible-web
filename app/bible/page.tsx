adb6a6aded6af99fc7658467556ecf9d72ef46ce
bruce.kim <bruce.kim@buzzvil.com>
2025-02-25 12:58:02 +0900

    update bible page.tsx

--- app/bible/page.tsx
+++ app/bible/page.tsx
@@ -5,15 +5,16 @@ const db = sql("free-bible-kor-nkrv.db");
 
 interface Verse {
   id: number;
-  book: string;
-  verse: string;
+  book: number;
+  chapter: number;
+  verse: number;
   content: string;
 }
 
 const getVerses = async (): Promise<Verse[]> => {
   await new Promise((resolve) => setTimeout(resolve, 2000)); // 지연
   // TODO FIX [ Server ] Error: no such table: bible
-  return db.prepare("SELECT id, book, verse, content FROM bible LIMIT 1").all() as Verse[]; // 데이터 가져오기
+  return db.prepare("SELECT id, book, chapter, verse, content FROM bible LIMIT 10").all() as Verse[]; // 데이터 가져오기
 };
 
 export default async function Page() {
@@ -22,10 +23,11 @@ export default async function Page() {
     <>
       <div className="flex flex-col gap-4 justify-center gap-4 px-5 md:px-10"> 
             {verses.map((verse) => (
-                <div key={verse.id}>
-                    <h1>{verse.book}</h1>
-                    <p>{verse.verse}</p>
-                    <p>{verse.content}</p>
+                <div className="flex" key={verse.id}>
+                    <h1 className="size-10 flex-non">{verse.book}</h1>
+                    <h2 className="size-10 flex-non">{verse.chapter}</h2>
+                    <p className="size-10 flex-non">{verse.verse}</p>
+                    <p className="size-10 flex-grow">{verse.content}</p>
                 </div>
             ))}
       </div>
