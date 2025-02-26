89459d08da2a2a29d7832f0a8b6b0e1869de6361
Hyunghui Kim <realwind2048@gmail.com>
2025-02-26 23:41:55 +0900

    Fix lint VerseCardForQuery

--- app/components/VerseCardForQuery.tsx
+++ app/components/VerseCardForQuery.tsx
@@ -7,9 +7,9 @@ import { useSearchParams } from 'next/navigation'
 
 export function VerseCardForQuery() {
     const searchParams = useSearchParams()
-    const imageSrc = searchParams.get('imageSrc') || '/images/bg/bg_1.webp'
-    const verseString = searchParams.get('verseString') || "예수께서 대답하여 이르시되 진실로 진실로 내게 이르노니 사람이 거듭나지 아니하면 하나님의 나라를 볼 수 없느니라"
-    const indexString = searchParams.get('indexString') || "요한복음 3:3"
+    const imageSrc = searchParams?.get('imageSrc') || '/images/bg/bg_1.webp'
+    const verseString = searchParams?.get('verseString') || "예수께서 대답하여 이르시되 진실로 진실로 내게 이르노니 사람이 거듭나지 아니하면 하나님의 나라를 볼 수 없느니라"
+    const indexString = searchParams?.get('indexString') || "요한복음 3:3"
     return (
         <VerseCardForProp 
             verseString={verseString}
