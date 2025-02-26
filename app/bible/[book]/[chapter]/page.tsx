170faa282ca5d35ed15e7e86afdc90663911c2dd
Hyunghui Kim <realwind2048@gmail.com>
2025-02-27 00:46:35 +0900

    Add 저작권 표시

--- app/bible/[book]/[chapter]/page.tsx
+++ app/bible/[book]/[chapter]/page.tsx
@@ -14,7 +14,7 @@ export default async function Page({ params }: { params: Promise<{ book: number,
   return (
     <>
       <div className="flex flex-col gap-2 justify-center p-5 md:p-10"> 
-            <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-blue-900 dark:text-blue-300">한글성경뱃지</span>
+            <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-blue-900 dark:text-blue-300">개역개정</span>
             <div className="flex gap-2 justify-center p-2 md:p-4">
               <h1 className="basis-auto">책 {book}</h1>
               <h2 className="basis-auto">챕터 {chapter}</h2>
@@ -29,6 +29,9 @@ export default async function Page({ params }: { params: Promise<{ book: number,
                   </li>
               </ol>
             ))}
+            <div className="flex gap-2 justify-center p-2 md:p-4">
+              <h1 className="basis-auto text-xs text-gray-200">"본서에 사용한 『성경전서 개역개정판』의 저작권은 재단법인 대한성서공회 소유이며 재단법인 대한성서공회의 허락을 받고 사용하였음"</h1>
+            </div>
       </div>
     </>
   )
