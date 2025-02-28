7d22d5510fb8af92c4fb82cce8cea126bb3db3b5
Hyunghui Kim <realwind2048@gmail.com>
2025-02-28 23:58:18 +0900

    Remove HeratBibleMainSignTextLogo from home

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,6 @@ export default function Page() {
   return (
     <>
       <div className="flex flex-col gap-4 justify-center gap-4 px-5 md:px-10"> 
-        <div>
-          <HeratBibleMainSignTextLogo />
-        </div>
         <div className="grid grid grid-cols-1 md:grid-cols-2 justify-center gap-4 mb-4">
           <EntryPointForVerseCard />
           <EntryPointForBible />
