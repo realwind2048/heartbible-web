371e316cdf515214975672dbba4dac24b3bcda3f
Hyunghui Kim <realwind2048@gmail.com>
2025-03-01 23:30:02 +0900

    Add sitemap_index.xml

---
+++ app/sitemap.tsx
@@ -0,0 +1,19 @@
+import type { MetadataRoute } from 'next'
+import { BASE_URL } from '@/libs/constants'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+        url: `${BASE_URL}`,
+        lastModified: new Date(),
+        changeFrequency: 'daily',
+        priority: 1,
+    },
+    {
+        url: `${BASE_URL}bible`,
+        lastModified: new Date(),
+        changeFrequency: 'daily',
+        priority: 0.8,
+    },
+  ]
+}
