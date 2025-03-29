bf3c48952acd2e33e4a8b0a4a0b0b6cc17e51761
bruce.kim <bruce.kim@buzzvil.com>
2025-03-29 20:05:39 +0900

    next.config.js 파일을 삭제하고, next.config.ts 파일에 API 리라이트 설정을 추가하여 코드 일관성을 높임.

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,12 @@ const nextConfig: NextConfig = {
     removeConsole: process.env.NODE_ENV === "production", // 프로덕션에서 콘솔 제거
     // removeConsole: false, // 콘솔 로그 완전히 제거
   },
+  rewrites: async () => [
+    {
+      source: '/api/sermon/:path*',
+      destination: 'http://localhost:8080/api/sermon/:path*'
+    },
+  ],
 };
 
 export default nextConfig;
